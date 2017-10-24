874fad772dd4df16fadba0e8396545bf684d6a4c
elisa <elisa1001940@gmail.com>
2017-10-24 15:40:51 +0800

    Scanner,name

--- src/com/elisa/Hello.java
+++ src/com/elisa/Hello.java
@@ -1,9 +1,21 @@
 package com.elisa;
 
+import java.util.Scanner;
+
 public class Hello {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+     System.out.println("請輸入你的名字:");
+     Scanner scanner = new Scanner(System.in);
+     String line = scanner.nextLine();
+     System.out.println("Hello," + line);
+
+     System.out.println("你幾歲呢?");
+     line = scanner.nextLine();
+     int age = Integer.parseInt(line);
+     System.out.println(age);
+     System.out.println(age > 25);
+     
 System.out.println("Hello World");
 Person p = new Person();
 p.sayHello();
--- src/com/elisa/VariableTester.java
+++ src/com/elisa/VariableTester.java
@@ -1,5 +1,7 @@
 package com.elisa;
 
+import java.util.Random;
+
 public class VariableTester {
 
 	public static void main(String[] args) {
@@ -10,6 +12,20 @@ char c2 = 'Z';
 System.out.println((int)c2);
 boolean pass = false;
 System.out.println(!pass);
+
+
+Random r = new Random();
+int n1 = r.nextInt(6)+1;
+int n2 = r.nextInt(6)+1;
+int n3 = r.nextInt(6)+1;
+int n4 = r.nextInt(6)+1;
+System.out.println(3 <= 2);
+
+String s = new String("Hello");
+String s2 = "Hahahahaha";
+int len = s.length();
+System.out.println("int:" + len);
+System.out.println(s.charAt(0));
 	}
 
 }
