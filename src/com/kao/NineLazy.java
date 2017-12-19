9d8e02387b49bb76ba09727a12f1ffb99303c77e
elisapig <elisa1001940@gmail.com>
2017-12-19 15:18:31 +0800

    期中複習

---
+++ src/com/kao/NineLazy.java
@@ -0,0 +1,24 @@
+package com.kao;
+
+public class NineLazy {
+
+	public static void main(String[] args) {
+		for (int i = 1; i <= 9; i++) {
+			for (int n = 2; n <= 9; n++) {
+				if (3 < i && i < 9) {
+					System.out.print("..." + "\t");
+				} else {
+
+					if ((i * n) < 10) {
+						System.out.print(n + "*" + i + "=" + " " + (n * i) + "\t");
+					} else {
+						System.out.print(n + "*" + i + "=" + (n * i) + "\t");
+
+					}
+				}
+
+			}
+			System.out.println("");
+		}
+	}
+}
