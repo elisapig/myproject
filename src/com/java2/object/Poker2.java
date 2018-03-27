ea446e01c2733af4a1d0668fca17836ae52d4b4a
elisapig <elisa1001940@gmail.com>
2018-03-27 16:32:19 +0800

    Poker2

--- src/com/java2/object/Poker2.java
+++ src/com/java2/object/Poker2.java
@@ -2,14 +2,19 @@ package com.java2.object;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Poker2 {
+	Random random = new Random();
 	String flowers ="SHDC";
 	List<Integer> cards = new ArrayList<>();
-	public Poker2(){
-		for(int i=0;i<cards.size();i++){
-			cards.add(i);
-			System.out.println();
+	
+	
+	public void shuffle(){
+		for(int i=0;i<52;i++){
+			int r = random.nextInt(52);
+			cards.set(i, r);
+			
 			
 		}
 	}
