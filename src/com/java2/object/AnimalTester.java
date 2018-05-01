a28196866e6f554f35f08bcd43c5286dff2678f6
elisapig <elisa100194@gmail.com>
2018-05-01 16:31:20 +0800

    stepik

---
+++ src/com/java2/object/AnimalTester.java
@@ -0,0 +1,27 @@
+package com.java2.object;
+
+public class AnimalTester {
+	
+
+		  public void say() {
+			  
+		  }
+		  
+		}
+		
+		class Cat extends AnimalTester {
+			@Override
+			public void say() {
+				System.out.println(meow-meow);	
+			}
+		}
+
+		class Dog extends AnimalTester {
+			System.out.println(arf-arf);	
+		}
+
+		class Duck extends AnimalTester {
+			System.out.println(quack-quack);	
+		}
+		
+
