d069f718450cee6179b16fc0ecd9099929146cd8
elisapig <elisa1001940>
2018-06-12 14:28:32 +0800

    修改

--- src/com/java2/oo/VendingMachine.java
+++ src/com/java2/oo/VendingMachine.java
@@ -3,22 +3,23 @@ package com.java2.oo;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+
 public class VendingMachine {
-	int total=0;
+	int total = 0;
 	List<Drink> drinks = new ArrayList<>();
-	
-	public void on(){
+
+	public void on() {
 		System.out.println("½Ð§ë¹ô.....");
-		for(int i=0;i<drinks.size();i++){
+		for (int i = 0; i < drinks.size(); i++) {
 			Drink drink = drinks.get(i);
-			System.out.println(drink.getId()+"\t"+drink.getName()+"\t"+drink.getPrice());
+			System.out.println(drink.getId() + "\t" + drink.getName() + "\t" + drink.getPrice());
 		}
-		
+
 		Scanner scanner = new Scanner(System.in);
 		int data = -1;
-		while(data !=0){
-			data=scanner.nextInt();
-			switch(data){
+		while (data != 0) {
+			data = scanner.nextInt();
+			switch (data) {
 			case 0:
 				break;
 			}
--- src/com/java2/oo/VendingMain.java
+++ src/com/java2/oo/VendingMain.java
@@ -5,23 +5,30 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
-
 public class VendingMain {
 
 	public static void main(String[] args) {
-		VendingMain vm = new VendingMain();
+		VendingMachine vm = new VendingMachine();
 		FileReader fr;
 		try {
-			fr =new FileReader("vending.txt");
+			fr = new FileReader("vending.txt");
 			BufferedReader in = new BufferedReader(fr);
 			String line = in.readLine();
-			String[] token =line.split(",");
+			String[] tokens = line.split(",");
+			int count = Integer.parseInt(tokens[0]);
+			for (int i = 0; i < count; i++) {
+				String name = tokens[i * 2 + 1];
+				int price = Integer.parseInt(tokens[i * 2 + 2]);
+				vm.drinks.add(new Drink(i + 1, name, price));
+			}
+			vm.on();
+
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
-		}catch (IOException e){
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 	}
 
 }
