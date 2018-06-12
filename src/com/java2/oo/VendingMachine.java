56e51c7ad392cd0d46e7b3a1aa4da95fa1717eff
elisapig <elisa1001940>
2018-06-12 15:16:40 +0800

    修改1

--- src/com/java2/oo/VendingMachine.java
+++ src/com/java2/oo/VendingMachine.java
@@ -1,8 +1,7 @@
 package com.java2.oo;
-
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class VendingMachine {
 	int total = 0;
@@ -11,7 +10,7 @@ public class VendingMachine {
 	public void on() {
 		System.out.println("請投幣.....");
 		for (int i = 0; i < drinks.size(); i++) {
-			Drink drink = drinks.get(i);
+			Drink drink = drinks.get(i);//輸出飲料+價錢
 			System.out.println(drink.getId() + "\t" + drink.getName() + "\t" + drink.getPrice());
 		}
 
--- src/com/java2/oo/VendingMain.java
+++ src/com/java2/oo/VendingMain.java
@@ -8,17 +8,19 @@ import java.io.IOException;
 public class VendingMain {
 
 	public static void main(String[] args) {
+		//讀取鍵盤輸入 BufferedReader buf  =  new BufferdReader (new InputStreamReader(System.in));
+		//readLine() 讀取檔案每行資料 傳回為String 自動刪除跳行符號回此字會湊在一起
 		VendingMachine vm = new VendingMachine();
 		FileReader fr;
 		try {
 			fr = new FileReader("vending.txt");
-			BufferedReader in = new BufferedReader(fr);
+			BufferedReader in = new BufferedReader(fr);//讀取"vending.txt"
 			String line = in.readLine();
-			String[] tokens = line.split(",");
-			int count = Integer.parseInt(tokens[0]);
+			String[] tokens = line.split(",");//代號以，作分類
+			int count = Integer.parseInt(tokens[0]);//3項飲料
 			for (int i = 0; i < count; i++) {
-				String name = tokens[i * 2 + 1];
-				int price = Integer.parseInt(tokens[i * 2 + 2]);
+				String name = tokens[i * 2 + 1];//基數項為名稱
+				int price = Integer.parseInt(tokens[i * 2 + 2]);//偶數項為價錢
 				vm.drinks.add(new Drink(i + 1, name, price));
 			}
 			vm.on();
