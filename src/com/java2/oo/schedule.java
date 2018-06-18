bdd9baf9e0cf92d2e592423c1debb00ac781fbaf
elisapig <elisa1001940@gmail.com>
2018-06-18 16:47:38 +0800

    practice3

--- src/com/java2/oo/schedule.java
+++ src/com/java2/oo/schedule.java
@@ -1,4 +1,9 @@
 package com.java2.oo;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 /*有一個文字檔儲存了表資料(schedule.txt)
  * 請讀取資料後，設計程式可檢查是否有空檔?
  * 如:
@@ -10,8 +15,18 @@ package com.java2.oo;
  * [有課: C004 English 到 16時]
  * */
 public class schedule {
-
+	List<Integer> days = new ArrayList<>();
+	int week;
+	int time;
+	String subject;
+	
+	public void schedule() {
+		
+	}
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("請輸入星期:");
+		System.out.println("請輸入時間:");
 		
 	}
 
